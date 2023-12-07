7a18fae18ce568b9ff18b9f2387e0f30b21ef652
Uriel Salischiker <uriels@spotify.com>
2023-12-07 23:09:23 +0100

    Stage #3: Respond with 404

--- src/main/kotlin/com/urielsalis/http/HttpServer.kt
+++ src/main/kotlin/com/urielsalis/http/HttpServer.kt
@@ -1,7 +1,32 @@
 package com.urielsalis.http
 
 object HttpServer {
+    val getHandlers = mapOf(
+        "/".toRegex() to ::handleRoot
+    )
+
     fun handle(request: Request): Response {
+        val response = when (request.method) {
+            Method.GET -> getHandler(request, getHandlers)
+            else -> null
+        }
+        if (response != null) {
+            return response
+        }
+        return Response(StatusCode.NOT_FOUND)
+    }
+
+    private fun getHandler(
+        request: Request, handlers: Map<Regex, (Request) -> Response>
+    ) = handlers.firstNotNullOfOrNull {
+        if (it.key.matches(request.path)) {
+            it.value(request)
+        } else {
+            null
+        }
+    }
+
+    private fun handleRoot(request: Request): Response {
         return Response(StatusCode.OK)
     }
 }
