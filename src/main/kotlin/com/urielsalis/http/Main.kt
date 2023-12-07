88ddfa52e75c646f034031b2dcaf1d879d835683
Uriel Salischiker <uriels@spotify.com>
2023-12-07 23:25:02 +0100

    Stage #6: Concurrent connections

--- src/main/kotlin/com/urielsalis/http/Main.kt
+++ src/main/kotlin/com/urielsalis/http/Main.kt
@@ -2,13 +2,17 @@ package com.urielsalis.http
 
 import java.io.IOException
 import java.net.ServerSocket
+import java.util.concurrent.Executors
 
 fun main() {
     try {
         val serverSocket = ServerSocket(4221)
         serverSocket.reuseAddress = true
-        val handler = ConnectionHandler(serverSocket.accept(), HttpServer::handle)
-        handler.handle()
+        val executor = Executors.newCachedThreadPool()
+        while (true) {
+            val handler = ConnectionHandler(serverSocket.accept(), HttpServer::handle)
+            executor.submit { handler.handle() }
+        }
     } catch (e: IOException) {
         throw RuntimeException(e)
     }
