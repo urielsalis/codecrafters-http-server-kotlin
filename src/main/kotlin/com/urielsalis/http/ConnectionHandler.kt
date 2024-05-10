177d32ce06bd71cb3650652bff92c876a09d0f6c
urielsalis <uriel@urielsalis.me>
2024-05-10 17:02:23 +0200

    HTTP Compression > Stage #3: Gzip compression

--- src/main/kotlin/com/urielsalis/http/ConnectionHandler.kt
+++ src/main/kotlin/com/urielsalis/http/ConnectionHandler.kt
@@ -1,6 +1,8 @@
 package com.urielsalis.http
 
+import java.io.ByteArrayOutputStream
 import java.net.Socket
+import java.util.zip.GZIPOutputStream
 
 class ConnectionHandler(clientSocket: Socket, val handler: (Request) -> Response) {
     private val input = clientSocket.getInputStream().bufferedReader()
@@ -23,14 +25,19 @@ class ConnectionHandler(clientSocket: Socket, val handler: (Request) -> Response
         }
         val request = parseRequest(lines, body)
         val response = handler(request)
-        if(request.isGzipAllowed()) {
+        if (request.isGzipAllowed()) {
             return write(response.gzipCompress())
         }
         write(response)
     }
 
     private fun Response.gzipCompress(): Response {
-        return this.copy(headers = this.headers + ("Content-Encoding" to "gzip"))
+        val compressedBody = body.gzipCompress()
+        val newHeaders =
+            this.headers.filterNot { it.key.lowercase() == "content-length" }
+                .plus("Content-Encoding" to "gzip")
+                .plus("Content-Length" to compressedBody.size.toString())
+        return this.copy(headers = newHeaders, body = compressedBody)
     }
 
     private fun parseRequest(
@@ -41,9 +48,7 @@ class ConnectionHandler(clientSocket: Socket, val handler: (Request) -> Response
         val method = Method.valueOf(firstLine[0])
         val path = firstLine[1]
         val headers =
-            lines.drop(1)
-                .takeWhile { it.isNotBlank() }
-                .map { it.split(": ") }
+            lines.drop(1).takeWhile { it.isNotBlank() }.map { it.split(": ") }
                 .associate { it[0] to it[1] }
         return Request(method, path, headers, body)
     }
@@ -56,9 +61,19 @@ class ConnectionHandler(clientSocket: Socket, val handler: (Request) -> Response
                     append("$key: $value\r\n")
                 }
                 append("\r\n")
-                response.body?.let { append(it) }
             }
         output.write(builder.toByteArray())
+        if (response.body.isNotEmpty()) {
+            output.write(response.body)
+        }
         output.flush()
     }
+}
+
+private fun ByteArray.gzipCompress(): ByteArray {
+    val bos = ByteArrayOutputStream()
+    val gzip = GZIPOutputStream(bos)
+    gzip.write(this)
+    gzip.finish()
+    return bos.toByteArray()
 }
--- src/main/kotlin/com/urielsalis/http/Domain.kt
+++ src/main/kotlin/com/urielsalis/http/Domain.kt
@@ -6,24 +6,38 @@ data class Request(
     val headers: Map<String, String>,
     val body: String?,
 ) {
-    fun isGzipAllowed() = headers.any {
-        it.key.lowercase() == "accept-encoding" && it.value.split(",").any { it.trim() == "gzip" }
-    }
+    fun isGzipAllowed() =
+        headers.any {
+            it.key.lowercase() == "accept-encoding" && it.value.split(",").any { it.trim() == "gzip" }
+        }
 }
 
 data class Response(
     val status: StatusCode,
     val headers: Map<String, String> = mapOf("Content-Length" to "0"),
-    val body: String? = null,
-)
+    val body: ByteArray = ByteArray(0),
+) {
+    constructor(
+        status: StatusCode,
+        headers: Map<String, String>,
+        body: String,
+    ) : this(status, headers, body.toByteArray())
+}
 
 enum class Method {
-    GET, POST, PUT, DELETE,
+    GET,
+    POST,
+    PUT,
+    DELETE,
 }
 
 enum class StatusCode(val code: Int, val message: String) {
-    OK(200, "OK"), CREATED(201, "Created"), BAD_REQUEST(400, "Bad Request"), NOT_FOUND(
-        404, "Not Found"
+    OK(200, "OK"),
+    CREATED(201, "Created"),
+    BAD_REQUEST(400, "Bad Request"),
+    NOT_FOUND(
+        404,
+        "Not Found",
     ),
     INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
 }
